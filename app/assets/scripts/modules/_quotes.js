3260e6afc02cfb865467b59ecb32a4119c80b88f
Osania <c5d59d07@opayq.com>
2017-10-29 17:01:58 +0000

    Quotetowiki (#23)
    * Link author of quote to Wikipedia page.
    
    Links to information about the author on Wikipedia.
    
    * Check for unknown authors
    
    If quote author is unknown, do not link to wikipedia
    
    * Cache quotes
    
    Set & get chrome local storage
    
    * Fix quote caching

--- app/assets/scripts/modules/_quotes.js
+++ app/assets/scripts/modules/_quotes.js
@@ -4,49 +4,100 @@ import $ from '../vendor/jquery-3.2.1.min';
 $("#tweetButton").hide();
 
 function checkCache(params) {
-    
+    return new Promise(function (resolve, reject) {
+        chrome.storage.local.get('quotes', function (params) {
+            if (params.quotes) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        })
+    }).catch(function (error) {
+        console.log('Quotes cache error: ', error);
+    })
+};
+
+function evListeners() {
+    // tweet quotes
+    $("#tweetButton").on("click", tweet);
+
+    //fade in author and twitter icon
+    $('.quotesBox').on('mouseover', function () {
+        $('#tweetButton').fadeIn("slow").show();
+        $('#quoteAuthor').fadeIn("slow").show();
+    });
+    $('.quotesBox').on('mouseleave', function () {
+        $('#tweetButton').fadeOut("slow");
+        $('#quoteAuthor').fadeOut("slow");
+    });
+}
+
+function loadQuotes() {
+    chrome.storage.local.get('quotes', function (quotes) {
+        displayQuote(quotes.quotes);
+        console.log('Quotes loaded from cache.');
+    })
+}
+
+function tweet() {
+    var randomQuote = document.getElementById('quoteText').textContent;
+    var randomAuthor = document.getElementById('quoteAuthor').textContent;
+    var tweetUrl = ' https://twitter.com/intent/tweet?text=' + encodeURIComponent(randomQuote) + " " + encodeURIComponent(randomAuthor) + " " + "-" + "Turtles 18";
+    window.open(tweetUrl);
+}
+
+function displayQuote(quote = []) {
+    var randomQuote = quote[Math.floor(Math.random() * 102)];
+    // Check for unknown authors
+    if (randomQuote.name.toLowerCase() === "unknown") {
+        $("#quoteText").append(randomQuote.quote);
+        $("#quoteAuthor").append(randomQuote.name).hide();
+    } else {
+        $("#quoteText").append(randomQuote.quote);
+        $("#quoteAuthor").append(`<a href="${randomQuote.wikiUrl}" target="_blank">${randomQuote.name}</a>`).hide();
+    }
+}
+// Store quotes to cache
+function storeQuotes(quotes) {
+    chrome.storage.local.set({
+        quotes: quotes
+    }, function () {
+        console.log('Quotes cached.');
+    });
 }
 
 // function to load random quotes from json file
 function quoteGenerator() {
-    $.getJSON("https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520", function(inspiringQuotes) {
-        // var to randomize order of array indexes
-        return new Promise(function (resolve, reject) { 
-            var quotes = [];
-            inspiringQuotes.forEach(function(quote) {
-                quote.wikiUrl = "https://en.wikipedia.org/wiki/" + encodeURI(quote.name);
-                quotes.push(quote);
-            }, this);
-            resolve(quotes);
-         }).then(function (data) { 
-            var randomQuote = data[Math.floor(Math.random() * 101)];
-
-            $("#quoteText").append(randomQuote.quote);
-            $("#quoteAuthor").append(`<a href="${randomQuote.wikiUrl}" target="_blank">${randomQuote.name}</a>`).hide();
-          }).catch(err => {
-              console.log(err);
-          });
-    });
-
-    // tweet quotes
-    $("#tweetButton").on("click", tweet);
-
-    function tweet() {
-        var randomQuote = document.getElementById('quoteText').textContent;
-        var randomAuthor = document.getElementById('quoteAuthor').textContent;
-        var tweetUrl = ' https://twitter.com/intent/tweet?text=' + encodeURIComponent(randomQuote) + " " + encodeURIComponent(randomAuthor) + " " + "-" + "Turtles 18";
-        window.open(tweetUrl);
-    }
-
-    //fade in author and twitter icon
-    $('.quotesBox').on('mouseover', function() {
-        $('#tweetButton').fadeIn("slow").show();
-        $('#quoteAuthor').fadeIn("slow").show();
-    });
-    $('.quotesBox').on('mouseleave', function() {
-        $('#tweetButton').fadeOut("slow");
-        $('#quoteAuthor').fadeOut("slow");
-    });
+    return new Promise(function (resolve, reject) {
+        resolve(checkCache());
+    }).then(function (cacheStatus) {
+        if (cacheStatus) {
+            // if there is cached quotes, load them and fire up event listeners
+            loadQuotes();
+            evListeners();
+        } else {
+            // else fetch quotes from github gist and load them
+            console.log('No quotes in cache. Loading quotes from gist.');
+            $.getJSON("https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520", function (inspiringQuotes) {
+                // var to randomize order of array indexes
+                return new Promise(function (resolve, reject) {
+                    var quotes = [];
+                    inspiringQuotes.forEach(function (quote) {
+                        quote.wikiUrl = "https://en.wikipedia.org/wiki/" + encodeURI(quote.name);
+                        quotes.push(quote);
+                    }, this);
+                    resolve(quotes);
+                }).then(function (data) {
+                    // store modified quotes to storage
+                    storeQuotes(data);
+                    displayQuote(data);
+                    evListeners();
+                }).catch(err => {
+                    console.log(err);
+                });
+            });
+        }
+    })
 }
 
 export default quoteGenerator;
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -2756,38 +2756,23 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 //hide button on load
 (0, _jquery2.default)("#tweetButton").hide();
 
-function checkCache(params) {}
-
-// function to load random quotes from json file
-function quoteGenerator() {
-    _jquery2.default.getJSON("https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520", function (inspiringQuotes) {
-        // var to randomize order of array indexes
-        return new Promise(function (resolve, reject) {
-            var quotes = [];
-            inspiringQuotes.forEach(function (quote) {
-                quote.wikiUrl = "https://en.wikipedia.org/wiki/" + encodeURI(quote.name);
-                quotes.push(quote);
-            }, this);
-            resolve(quotes);
-        }).then(function (data) {
-            var randomQuote = data[Math.floor(Math.random() * 101)];
-
-            (0, _jquery2.default)("#quoteText").append(randomQuote.quote);
-            (0, _jquery2.default)("#quoteAuthor").append("<a href=\"" + randomQuote.wikiUrl + "\" target=\"_blank\">" + randomQuote.name + "</a>").hide();
-        }).catch(function (err) {
-            console.log(err);
+function checkCache(params) {
+    return new Promise(function (resolve, reject) {
+        chrome.storage.local.get('quotes', function (params) {
+            if (params.quotes) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
         });
+    }).catch(function (error) {
+        console.log('Quotes cache error: ', error);
     });
+};
 
+function evListeners() {
     // tweet quotes
     (0, _jquery2.default)("#tweetButton").on("click", tweet);
-
-    function tweet() {
-        var randomQuote = document.getElementById('quoteText').textContent;
-        var randomAuthor = document.getElementById('quoteAuthor').textContent;
-        var tweetUrl = ' https://twitter.com/intent/tweet?text=' + encodeURIComponent(randomQuote) + " " + encodeURIComponent(randomAuthor) + " " + "-" + "Turtles 18";
-        window.open(tweetUrl);
-    }
 
     //fade in author and twitter icon
     (0, _jquery2.default)('.quotesBox').on('mouseover', function () {
@@ -2797,6 +2782,76 @@ function quoteGenerator() {
     (0, _jquery2.default)('.quotesBox').on('mouseleave', function () {
         (0, _jquery2.default)('#tweetButton').fadeOut("slow");
         (0, _jquery2.default)('#quoteAuthor').fadeOut("slow");
+    });
+}
+
+function loadQuotes() {
+    chrome.storage.local.get('quotes', function (quotes) {
+        displayQuote(quotes.quotes);
+        console.log('Quotes loaded from cache.');
+    });
+}
+
+function tweet() {
+    var randomQuote = document.getElementById('quoteText').textContent;
+    var randomAuthor = document.getElementById('quoteAuthor').textContent;
+    var tweetUrl = ' https://twitter.com/intent/tweet?text=' + encodeURIComponent(randomQuote) + " " + encodeURIComponent(randomAuthor) + " " + "-" + "Turtles 18";
+    window.open(tweetUrl);
+}
+
+function displayQuote() {
+    var quote = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+    var randomQuote = quote[Math.floor(Math.random() * 102)];
+    // Check for unknown authors
+    if (randomQuote.name.toLowerCase() === "unknown") {
+        (0, _jquery2.default)("#quoteText").append(randomQuote.quote);
+        (0, _jquery2.default)("#quoteAuthor").append(randomQuote.name).hide();
+    } else {
+        (0, _jquery2.default)("#quoteText").append(randomQuote.quote);
+        (0, _jquery2.default)("#quoteAuthor").append('<a href="' + randomQuote.wikiUrl + '" target="_blank">' + randomQuote.name + '</a>').hide();
+    }
+}
+// Store quotes to cache
+function storeQuotes(quotes) {
+    chrome.storage.local.set({
+        quotes: quotes
+    }, function () {
+        console.log('Quotes cached.');
+    });
+}
+
+// function to load random quotes from json file
+function quoteGenerator() {
+    return new Promise(function (resolve, reject) {
+        resolve(checkCache());
+    }).then(function (cacheStatus) {
+        if (cacheStatus) {
+            // if there is cached quotes, load them and fire up event listeners
+            loadQuotes();
+            evListeners();
+        } else {
+            // else fetch quotes from github gist and load them
+            console.log('No quotes in cache. Loading quotes from gist.');
+            _jquery2.default.getJSON("https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520", function (inspiringQuotes) {
+                // var to randomize order of array indexes
+                return new Promise(function (resolve, reject) {
+                    var quotes = [];
+                    inspiringQuotes.forEach(function (quote) {
+                        quote.wikiUrl = "https://en.wikipedia.org/wiki/" + encodeURI(quote.name);
+                        quotes.push(quote);
+                    }, this);
+                    resolve(quotes);
+                }).then(function (data) {
+                    // store modified quotes to storage
+                    storeQuotes(data);
+                    displayQuote(data);
+                    evListeners();
+                }).catch(function (err) {
+                    console.log(err);
+                });
+            });
+        }
     });
 }
 
